f18c0981e4c290b85d9b169dddd453d504ca75c7
Dharika Manocha <dharika6688@gmail.com>
2021-08-17 05:44:03 +0530

    init

---
+++ src/components/Character.jsx
@@ -0,0 +1,36 @@
+import{ Card, makeStyles, Tooltip, Typography } from '@material-ui/core';
+
+
+const useStyle = makeStyles({
+    img: {
+        height: 400,
+        width: '100%',
+        objectFit: 'cover'
+    }
+})
+
+const Character = ({ item }) => {
+    const classes = useStyle();
+    return (
+        <Card>
+            <Tooltip title={
+                    <>
+                        <Typography>Name: {item.name}</Typography>
+                        <Typography>Real Name: {item.portrayed}</Typography>
+                        <Typography>Nick Name: {item.nickname}</Typography>
+                        <Typography>Birthday: {item.birthday}</Typography>
+                        <Typography>Status: {item.status}</Typography>
+                        
+                    </>
+                }
+                arrow placement="top">
+            
+                <img src={item.img} className={classes.img} alt="character"/>
+                
+            </Tooltip>
+        </Card>
+    )
+
+}
+
+export default Character;
---
+++ src/components/Characters.jsx
@@ -0,0 +1,23 @@
+import Character from './Character';
+import { Grid, Box } from '@material-ui/core' 
+
+
+
+const Characters = ( {data}) => {
+    return(
+        <Box style={{margin:15}}>
+            <Grid container spacing={4}>
+                {
+                    data.map(item=>(
+                        <Grid item xs={3}>
+                            <Character key = {item.char_id} item={item} />
+                        </Grid>
+                    ))
+                }
+
+            </Grid>
+        </Box>
+    )
+}
+
+export default Characters;
